ed3187a368c2b68e2fceb738cde1b866b1743dc7
Jennifer Beatty <jbeatty@southlandind.com>
2014-02-20 05:23:10 -0500

    Finishing up my code for this assignment. Includes styling updates and content updates.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+$().ready(function() {
 
+  $("button").click(function(){
+  if (localStorage.buttonClick) {
+   localStorage.buttonClick=Number(localStorage.buttonClick)+1;
+   document.getElementById("result").innerHTML = "<em>You have clicked the button " + localStorage.buttonClick + " time(s).</em>";
+    }
+    else{
+     localStorage.buttonClick=1;
+     document.getElementById("result").innerHTML = "<em>You have clicked the button " + localStorage.buttonClick + " time(s).</em>";
+}
+
+});
+
+  $(".nostore").click(function(){
+    localStorage.clear();
+    document.getElementById("result").innerHTML = "<em>You have clicked the button " + localStorage.buttonClick + " time(s).</em>";
+    
+  });
+
+
+});
